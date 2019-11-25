0301d493253cbd8f16fb8edb91553c5672e24b82
Alin Florin <alin@qoffice.ro>
2019-11-25 16:20:43 +0200

    -

--- DwLang.Language/Interpreter/BinaryExpressionEvaluator.cs
+++ DwLang.Language/Interpreter/BinaryExpressionEvaluator.cs
@@ -16,7 +16,18 @@ namespace DwLang.Language.Interpreter
             var right = (DwLangInterpreter.Evaluators[casted.Right.GetType()].Evaluate(casted.Right, ctx) as Constant).Value;
             switch (casted.OperatorType)
             {
-                
+                case BinaryOperatorType.Divide:
+                    return new Constant(left / right);
+                case BinaryOperatorType.Minus:
+                    return new Constant(left - right);
+                case BinaryOperatorType.Multiply:
+                    return new Constant(left * right);
+                case BinaryOperatorType.Plus:
+                    return new Constant(left + right);
+                case BinaryOperatorType.Pow:
+                    return new Constant(new BigDecimal(Math.Pow(left.ToInt64(), right.ToInt64()), ctx.GetMathContext()));
+                case BinaryOperatorType.Prm:
+                    return null; // TODO reuse expressions
             }
             return null;
         }
