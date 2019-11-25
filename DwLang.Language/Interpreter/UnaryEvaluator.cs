a03e2028be318f266ab36fea6f809ce1ba38e9b3
Alin Florin <alin@qoffice.ro>
2019-11-25 16:29:55 +0200

    -

---
+++ DwLang.Language/Interpreter/UnaryEvaluator.cs
@@ -0,0 +1,34 @@
+﻿using Deveel.Math;
+using DwLang.Language.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DwLang.Language.Interpreter
+{
+    [ExpressionEvaluator(typeof(UnaryExpression))]
+    public class UnaryEvaluator : IExpressionEvaluator
+    {
+        public Expression Evaluate(Expression expression, ExecutionContext ctx)
+        {
+            var casted = expression as UnaryExpression;
+            var value = (DwLangInterpreter.Evaluators[casted.Operand.GetType()].Evaluate(casted.Operand, ctx) as Constant).Value;
+            switch (casted.OperatorType)
+            {
+                case UnaryOperatorType.Factorial:
+                    return new Constant(Factorial(value, 2)); // TODO replace 2
+            }
+            return null;
+        }
+
+        private BigDecimal Factorial(BigDecimal value, BigDecimal n)
+        {
+            if (n.Equals(BigDecimal.One))
+            {
+                return value;
+            }
+            BigDecimal lessOne = n - BigDecimal.One;
+            return Factorial(value * lessOne, lessOne);
+        }
+    }
+}
