19b6948527ad1a95e4e4aa678962eaa0d0afbf2f
Alin Florin <alin@qoffice.ro>
2019-11-25 17:30:12 +0200

    -

---
+++ DwLang.Language/Interpreter/VarArgsEvaluator.cs
@@ -0,0 +1,31 @@
+﻿using DwLang.Language.Expressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DwLang.Language.Interpreter
+{
+    [ExpressionEvaluator(typeof(VarArgsExpression))]
+    public class VarArgsEvaluator : IExpressionEvaluator
+    {
+        public Expression Evaluate(Expression expression, ExecutionContext ctx)
+        {
+            var casted = expression as VarArgsExpression;
+            switch(casted.OperatorType)
+            {
+                case VarArgsOperatorType.Avg:
+                    var sumExpr = casted.Expressions.First();
+                    for (var i = 1; i < casted.Expressions.Count; i++)
+                    {
+                        sumExpr = new BinaryExpression(sumExpr, BinaryOperatorType.Plus, casted.Expressions.ElementAt(i));
+                    }
+                    return new BinaryExpression(sumExpr, BinaryOperatorType.Divide, new Constant(new Deveel.Math.BigDecimal(casted.Expressions.Count)));
+
+                case VarArgsOperatorType.Med:
+                    return null;
+            }
+            return null;
+        }
+    }
+}
