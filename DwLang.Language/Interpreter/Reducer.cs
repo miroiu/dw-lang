bed2201096634e1cde1e662a8a1167bab9f8a119
Alin Florin <alin@qoffice.ro>
2019-11-26 15:57:35 +0200

    -

--- DwLang.Language/Interpreter/Reducer.cs
+++ DwLang.Language/Interpreter/Reducer.cs
@@ -9,17 +9,11 @@ namespace DwLang.Language.Interpreter
     {
         public static Expression Reduce(Expression e, ExecutionContext ctx)
         {
-            try
+            if (e == null || e is Constant)
             {
-                if (e == null || e is Constant)
-                {
-                    return e;
-                }
-                return Reduce(DwLangInterpreter.Evaluators[e.GetType()].Evaluate(e, ctx), ctx);
-            } catch (Exception ex)
-            {
-                throw new DwLangExecutionException(ex, e);
+                return e;
             }
+            return Reduce(DwLangInterpreter.Evaluators[e.GetType()].Evaluate(e, ctx), ctx);
         }
     }
 }
