349d27aa904a7f83d9b3dcf26e725f834da397ca
Emanuel Miroiu <miroiu.emanuel@yahoo.com>
2019-11-28 15:59:46 +0200

    -

--- Publish.bat
+++ Publish.bat
@@ -1,1 +1,1 @@
-dotnet publish -c Release -r win-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true -o pub DwLang
+dotnet publish -c Release --framework netcoreapp3.0 -o pub DwLang /p:DebugType=None
