be4289a6f1ac43d7fc9d6ea6e9cf1060d287ee5f
Emanuel Miroiu <miroiu.emanuel@yahoo.com>
2019-11-28 18:25:59 +0200

    Adding REPL

--- DwLang/App.xaml.cs
+++ DwLang/App.xaml.cs
@@ -1,17 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using DwLang.Language;
+using System.IO;
 using System.Windows;
 
 namespace DwLang
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public DwLangReplConsole Console = new DwLangReplConsole();
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            if (e.Args.Length == 0)
+            {
+                MainWindow = new MainWindow();
+                MainWindow.Show();
+            }
+            else if (e.Args.Length > 1)
+            {
+                Console.WriteLine("Only one file at a time is allowed.");
+            }
+            else
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), e.Args[0]);
+
+                if (e.Args[0] == "repl")
+                {
+                    string code;
+                    do
+                    {
+                        Console.WriteLine("> ");
+                        code = Console.ReadLine();
+
+                        RunCode(code, true);
+                    }
+                    while (code != "exit");
+                }
+                else if (File.Exists(path))
+                {
+                    var source = File.ReadAllText(path);
+
+                    RunCode(source, false);
+                    Shutdown();
+                }
+                else
+                {
+                    System.Console.WriteLine($"File {e.Args[0]} does not exist.");
+                    Shutdown();
+                }
+            }
+        }
+
+        private void RunCode(string source, bool repl)
+        {
+            var preLexer = new DwLangPreLexer(source);
+            var stream = preLexer.Sanitize();
+            var lexer = new DwLangLexer(stream);
+            var parser = new DwLangParser(lexer);
+
+            var interpreter = new DwLangInterpreter(Console);
+            interpreter.Run(parser);
+        }
     }
 }
---
+++ DwLang/DwLangReplConsole.cs
@@ -0,0 +1,17 @@
+﻿using DwLang.Language;
+using System;
+
+namespace DwLang
+{
+    public class DwLangReplConsole : IOutputStream
+    {
+        public void WriteLine(string line)
+            => Console.WriteLine(line);
+
+        public void Clear()
+            => Console.Clear();
+
+        public string ReadLine()
+            => Console.ReadLine();
+    }
+}
